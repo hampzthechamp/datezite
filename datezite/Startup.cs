b7ffcdd7c870f7144de2bf078b7b94a4c4744682
hampan <h.torpman@gmail.com>
2018-12-30 18:51:16 +0100

    Add project files.

---
+++ datezite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(datezite.Startup))]
+namespace datezite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
