c6f6b05573943cf81161e5ec65737fecf3d6eea2
Tobias <tubbeh_97@hotmail.com>
2019-01-02 15:53:23 +0100

    lagt till en till layout, som ska användas på alla sidor där man är inloggad.

---
+++ datezite/Controllers/HeaderSignedInController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace datezite.Controllers
+{
+    public class HeaderSignedInController : Controller
+    {
+        // GET: HeaderSignedIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
