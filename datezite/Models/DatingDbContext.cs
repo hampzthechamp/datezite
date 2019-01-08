b28c6e49bd1857da328b55c18d64a7dab7fc8eca
hampan <h.torpman@gmail.com>
2019-01-08 18:23:44 +0100

    Revert "fel"
    This reverts commit aa4c10a82f51a603f8eb986a79193b2d16355aed.

---
+++ datezite/Models/DatingDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace datezite.Models
+{
+    public class DatingDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DatingDbContext(): base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static DatingDbContext Create()
+        {
+            return new DatingDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicationUser>().HasMany(x => x.Vänner).WithMany()
+                .Map(x => x.ToTable("Friends"));
+            modelBuilder.Entity<ApplicationUser>().HasMany(x => x.Inlägg).WithRequired(x => x.Author);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Entry> Entries { get; set; }
+
+       
+    }
+}
