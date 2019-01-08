2b1cb1ead127f8cff447dcb325e00aebef486461
hampan <h.torpman@gmail.com>
2019-01-08 18:22:46 +0100

    ölkjlk

---
+++ datezite/Models/GetApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+
+namespace datezite.Models
+{
+    public class GetApplicationUser
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public GetApplicationUser()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        
+
+        public ApplicationUser GetUserByName(string name)
+        {
+            var appUser = _context.Users.Single(u => u.UserName == name);
+
+
+            return appUser;
+        }
+    }
+}
